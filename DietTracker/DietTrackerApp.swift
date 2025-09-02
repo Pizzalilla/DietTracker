cb876e1fef4c9a546c7875293d359ce02c9f0647
Kartikay Singh <cmchilledbs@gmail.com>
2025-09-02 13:38:21 +1000

    Initial Commit

---
+++ DietTracker/DietTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietTrackerApp.swift
+//  DietTracker
+//
+//  Created by Kartikay Singh on 2/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct DietTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DietTrackerTests/DietTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DietTrackerTests.swift
+//  DietTrackerTests
+//
+//  Created by Kartikay Singh on 2/9/2025.
+//
+
+import Testing
+@testable import DietTracker
+
+struct DietTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
